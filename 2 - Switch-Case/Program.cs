bb929a4eac592be5ab45cb9e30a11db8886091c1
Marcos Vinicius <73191174+MarcosVinicius2003@users.noreply.github.com>
2021-01-23 14:41:54 -0300

    Add files via upload

---
+++ 2 - Switch-Case/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CalculadoraSimplesTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pergunta a operação
+            Console.WriteLine("Qual a operação deseja realizar? \nSoma, Subtracao, Multiplicao, Divisao");
+            string operacao = Console.ReadLine().ToLower();
+
+            // Pedir o 1º numero
+            Console.WriteLine("Digite o 1º numero: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            // Pedir o 2º 
+            Console.WriteLine("Digite o 2º numero: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            // Fazer o cálculo
+            float resultado = 0;
+        
+            // if(operacao == "Soma")
+            // {
+            //     resultado = num1 + num2;
+            // }
+            // else if(operacao == "Subtracao")
+            // {
+            //     resultado = num1 - num2;   
+            // }
+            // else if(operacao == "Multiplicao")
+            // {
+            //     resultado = num1 * num2;
+            // } 
+            // else if(operacao == "Divisao")
+            // { 
+            //     resultado = num1 / num2;
+            // }
+            // else{
+            //     Console.WriteLine("Operação Inválida");
+            // }
+
+            switch(operacao){
+
+                case "soma":
+                    resultado = num1 + num2;
+                    break;
+
+                case "subtracao":
+                    resultado = num1 - num2;
+                    break;
+
+                case "multiplicacao":
+                    resultado = num1 * num2;
+                    break;
+
+                case "divisao":
+                    resultado = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Operação Inválida! :(");
+                    break;
+            }
+
+            // Mostrar o resultado
+
+            //Concatenação
+            //Console.WriteLine("Calculo :"+ num1 +" com "+ num2 +" ="+ resultado);
+
+            // Interpolação
+            Console.WriteLine($"Calculo : {num1} com {num2} = {resultado}");
+
+        }
+    }
+}
---
+++ 3 - Laços while, do while , for/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _17_11___Laços_while__do_while___for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseja ver a tabuada de qual numero?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 0; contador <= 10; contador++)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+
+
+            // do
+            // {
+            //     // Executado pelo menos uma vez
+            //     Console.WriteLine("Bom Dia!");
+
+            //     Console.WriteLine("Ainda deseja receber um bom dia? ");
+
+            //     numeroDeVezes++;
+
+            //     resposta = Console.ReadLine();
+
+            // } while(resposta == "Sim");
+
+            // while(resposta == "Sim") {
+            //     // Executando quando condição é verdadeira
+            //     Console.WriteLine("Bom Dia!!");
+
+            //     // numeroDeVezes = numeroDeVezes + 1;
+            //     numeroDeVezes++;
+
+            //     Console.WriteLine("Ainda deseja receber um bom dia?");
+            //     resposta = Console.ReadLine();
+            // }
+
+            // Console.WriteLine($"Numero de vezes: {numeroDeVezes}");
+            //  Console.WriteLine("Fim Da Programação!");
+        }
+    }
+}
