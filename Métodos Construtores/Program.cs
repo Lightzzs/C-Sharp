4eddd9127d9ca919ab23e548b1c281236958163b
Marcos Vinicius <73191174+MarcosVinicius2003@users.noreply.github.com>
2021-01-24 00:19:56 -0300

    Add files via upload

---
+++ Métodos Construtores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Métodos_Construtores.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            string nome;
+            string descricao;
+            int estoque;
+
+            Console.WriteLine("Cadastro de produtos");
+
+            Console.WriteLine("Codigo de produto: ");
+            codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do produto: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Descrição do produto: ");
+            descricao = Console.ReadLine();
+
+            Console.Write("Quantidade no estoque: ");
+            estoque = int.Parse(Console.ReadLine());
+
+            produtos produto1 = new produtos(codigo, nome, descricao, estoque);
+
+            Console.WriteLine("\nInformações do produto:");
+            Console.WriteLine("");
+            Console.WriteLine($"Código: {produto1.Codigo}");
+            Console.WriteLine($"Nome: {produto1.Nome}");
+            Console.WriteLine($"Descrição: {produto1.Descricao}");
+            Console.WriteLine($"Estoque: {produto1.Estoque}");
+            Console.WriteLine("");
+        }
+    }
+}
